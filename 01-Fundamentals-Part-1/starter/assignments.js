9e11ff4b5db2e0b00f6311d4d56b8fa62fe0dc00
Yasser Shalash <yasservanshalash@gmail.com>
2021-12-12 20:26:51 +0100

    values and variables exercise

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,9 @@
+console.log("linked");
+
+let country = "Syria";
+let continent = "Asia";
+let population = 17500000;
+
+console.log("country", country);
+console.log("continent", continent);
+console.log("country", population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,12 @@
-// let js = "amazing";
-// if (js === "amazing") { alert("JavaScript is amazing"); }
+// // let js = "amazing";
+// // if (js === "amazing") { alert("JavaScript is amazing"); }
 
-console.log("linked");
+// console.log("linked");
 
-console.log("part 2");
+// console.log("part 2");
 
-console.log("why do i always go back to the beginning??!...");
+// console.log("why do i always go back to the beginning??!...");
 
-let firstName = "Yasser";
+// let firstName = "Yasser";
 
-console.log(firstName);
+// console.log(firstName);
