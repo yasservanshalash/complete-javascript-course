2104587ad75a6065fb9e6efbbda3d193aa1a11ea
Yasser Shalash <yasservanshalash@gmail.com>
2021-12-12 21:08:46 +0100

    data types exercise

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,5 +1,7 @@
 console.log("linked");
 
+//  =========== Values and Variables ===========
+
 let country = "Syria";
 let continent = "Asia";
 let population = 17500000;
@@ -7,3 +9,16 @@ let population = 17500000;
 console.log("country", country);
 console.log("continent", continent);
 console.log("country", population);
+
+
+
+//  =========== Data Types ===========
+
+
+let isIsland = false;
+let language;
+
+console.log("isIsland", isIsland);
+console.log("population", population);
+console.log("country", country);
+console.log("language", language);
