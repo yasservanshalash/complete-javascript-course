89447b2c779b2b6ae5b12d2cc1ff657abdd7b00e
Yasser Shalash <yasservanshalash@gmail.com>
2021-12-12 20:12:11 +0100

    so excited to begin

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,12 @@
-let js = "amazing";
-if (js === "amazing") { alert("JavaScript is amazing"); }
+// let js = "amazing";
+// if (js === "amazing") { alert("JavaScript is amazing"); }
 
-console.log("linked")
+console.log("linked");
+
+console.log("part 2");
+
+console.log("why do i always go back to the beginning??!...");
+
+let firstName = "Yasser";
+
+console.log(firstName);
