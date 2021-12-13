db2bc92860fdc223bed9af03ee580d82f78e625f
Yasser Shalash <yasservanshalash@gmail.com>
2021-12-13 10:28:29 +0100

    challenge1 added

---
+++ 01-Fundamentals-Part-1/starter/challenge1.js
@@ -0,0 +1,30 @@
+// Case 1:
+let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let johnHeight = 1.95;
+
+function bmi(mass, height) {
+    return mass / (height ** 2);
+} 
+
+markBMI = bmi(markMass, markHeight);
+johnBMI = bmi(johnMass, johnHeight);
+
+markHigherBMI = markBMI > johnBMI
+
+// Case 2:
+
+let markMass = 95;
+let markHeight = 1.88;
+let johnMass = 85;
+let johnHeight = 1.76;
+
+function bmi(mass, height) {
+    return mass / (height ** 2);
+} 
+
+markBMI = bmi(markMass, markHeight);
+johnBMI = bmi(johnMass, johnHeight);
+
+markHigherBMI = markBMI > johnBMI
