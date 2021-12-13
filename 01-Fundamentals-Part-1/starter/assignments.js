b86c0463af882cf327f645178d6b2f882760083e
Yasser Shalash <yasservanshalash@gmail.com>
2021-12-13 13:17:28 +0100

    equality operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -83,3 +83,38 @@ if (130000000 > 33000000) {
 } else {
     console.log(`${country}'s population is ${33000000 - 130000000} below average`)
 }
+
+
+
+//  =========== Type Conversion and Coercion ===========
+
+'9' - '5';  // 4
+'19' - '13' + '17'; // 617
+'19' - '13' + 17; // 23
+'123' < 57; // false
+5 + 6 + '4' + 9 - 4 - 2; //1143
+
+
+//  ===========  Equality Operators: == vs. === ===========
+
+
+// let numNeighbours = prompt('How many neighbour countries does your country have?');
+
+// if(numNeighbours == 1) {
+//     console.log("Only 1 border!");
+// } else if(numNeighbours > 1) {
+//     console.log("More than 1 border");
+// } else {
+//     console.log("No borders");
+// }
+
+
+let numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+if(numNeighbours === 1) {
+    console.log("Only 1 border!");
+} else if(numNeighbours > 1) {
+    console.log("More than 1 border");
+} else {
+    console.log("No borders");
+}
