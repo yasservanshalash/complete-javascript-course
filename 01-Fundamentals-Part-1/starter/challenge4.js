3884e3b9de3cfecac6abc9f3916e0c7e0d34b283
Yasser Shalash <yasservanshalash@gmail.com>
2021-12-13 16:32:38 +0100

    final touch challenge 4

--- 01-Fundamentals-Part-1/starter/challenge4.js
+++ 01-Fundamentals-Part-1/starter/challenge4.js
@@ -1,6 +1,6 @@
 let billValue = 275;
 let tipPercentage = billValue >= 50 && billValue <= 300 ? 15 : 20;
 let tipValue = billValue * (tipPercentage / 100); 
-let total = billValue + (billValue * (tipPercentage / 100));
+let total = billValue + tipValue;
 
 console.log(`The bill was ${billValue} and tip was ${tipValue} and total was ${total}`);
