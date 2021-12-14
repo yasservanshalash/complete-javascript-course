d26367d319179fc5835cb7c5a610b435f7d2cc14
Yasser Shalash <yasservanshalash@gmail.com>
2021-12-14 01:14:32 +0100

    second exercise done

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,3 +1,6 @@
+// Functions
+
+
 function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} people and it's capital city is ${capitalCity}`
 }
@@ -9,3 +12,23 @@ let finland = describeCountry("Finland", 6000000, "Helsinki");
 console.log(syria);
 console.log(lebanon);
 console.log(finland);
+
+// Function declarations vs expressions
+
+worldPopulation = 7900000000;
+function percentageOfWorld1(population) {
+    return (population * 100)/7900000000;
+}
+
+console.log(`The population of China makes about ${percentageOfWorld1(1441000000)}% of the world's population`);
+console.log(`The population of Syria makes about ${percentageOfWorld1(17500000)}% of the world's population`);
+console.log(`The population of Finland makes about ${percentageOfWorld1(6000000)}% of the world's population`);
+
+const percentageOfWorld2 = function(population){
+    return (population * 100)/7900000000;
+}
+
+console.log(`The population of China makes about ${percentageOfWorld2(1441000000)}% of the world's population`);
+console.log(`The population of Syria makes about ${percentageOfWorld2(17500000)}% of the world's population`);
+console.log(`The population of Finland makes about ${percentageOfWorld2(6000000)}% of the world's population`);
+
