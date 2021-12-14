1f2e5793cf7c29843f7bf4d53aef71523bdce1bd
Yasser Shalash <yasservanshalash@gmail.com>
2021-12-14 01:02:46 +0100

    first assignment editted

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,3 +1,11 @@
 function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} people and it's capital city is ${capitalCity}`
 }
+
+let syria = describeCountry("Syria", 17500000, "Damascus");
+let lebanon = describeCountry("Lebanon", 4000000, "Beirut");
+let finland = describeCountry("Finland", 6000000, "Helsinki");
+
+console.log(syria);
+console.log(lebanon);
+console.log(finland);
